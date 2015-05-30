d90eadb1a2421bf96bc1a68ce80ea0fdad63d087
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2015-05-30 18:59:15 -0300

    Add config for coolline
    Pry::Coolline needs a config to be deactivated since it is a bit buggy.
    By default it is deactivated but can easily be activated with
    `JazzFingers.coolline = true`.
    
    closes #5

--- lib/jazz_fingers.rb
+++ lib/jazz_fingers.rb
@@ -2,17 +2,20 @@ require "jazz_fingers/version"
 require "readline"
 require "awesome_print"
 require "pry"
-require "pry-coolline"
 require "pry-doc"
 require "pry-git"
 require "pry-remote"
 require "jazz_fingers/hirb_ext"
 
 module JazzFingers
+  autoload :Input, "jazz_fingers/input"
   autoload :Print, "jazz_fingers/print"
   autoload :Prompt, "jazz_fingers/prompt"
 
   class << self
+    attr_writer :colored_prompt, :prompt_separator, :coolline
+    attr_accessor :_hirb_output
+
     def print
       @print ||= Print.config
     end
@@ -22,7 +25,9 @@ module JazzFingers
       @prompt.config
     end
 
-    ### Options ###
+    def input
+      @input ||= Input.config
+    end
 
     # Color the prompt?
     #
@@ -33,29 +38,24 @@ module JazzFingers
     # widths with color codes when using \001 and \002 hints. 'false' for
     # libedit-based wrapper (standard on OS X unless ruby is explicitly compiled
     # otherwise).
-    #
     def colored_prompt
       @colored_prompt ||= (Readline::VERSION !~ /EditLine/) && Pry.color
     end
-
-    attr_writer :colored_prompt
 
     # Separator between application name and input in the prompt.
     #
     # Default: right angle quote, or '>' when using rb-readline which doesn't
     # handle mixed encodings well.
-    #
     def prompt_separator
       @prompt_separator ||= defined?(RbReadline) ? ">" : "\u00BB"
     end
 
-    attr_writer :prompt_separator
-
-    ### Internal methods ###
-
-    attr_accessor :_hirb_output
+    def coolline?
+      @coolline ||= false
+    end
   end
 end
 
 Pry.print = JazzFingers.print
 Pry.prompt = JazzFingers.prompt
+Pry.input = JazzFingers.input if JazzFingers.coolline?
---
+++ lib/jazz_fingers/input.rb
@@ -0,0 +1,27 @@
+require "pry-coolline"
+require "coderay"
+
+module JazzFingers
+  class Input
+    class << self
+      def config
+        {
+          cool: cool_input
+        }
+      end
+
+      def cool_input
+        Coolline.new do |c|
+          c.transform_proc = proc do
+            CodeRay.scan(c.line, :ruby).term
+          end
+
+          c.completion_proc = proc do
+            word = c.completed_word
+            Object.constants.map(&:to_s).select { |w| w.start_with? word }
+          end
+        end
+      end
+    end
+  end
+end
