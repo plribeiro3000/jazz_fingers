776b658728884d6ac05ab745c7bf6c80e7e93691
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2015-05-31 21:10:09 -0300

    Add alias to some commands

--- lib/jazz_fingers/setup.rb
+++ lib/jazz_fingers/setup.rb
@@ -1,3 +1,9 @@
+# JazzFingers dance
 Pry.print = JazzFingers.print if JazzFingers.config.awesome_print?
 Pry.prompt = JazzFingers.prompt
 Pry.input = JazzFingers.input if JazzFingers.config.coolline?
+
+# Command Shortcuts
+Pry.commands.alias_command('c', 'continue')
+Pry.commands.alias_command('s', 'step')
+Pry.commands.alias_command('n', 'next')
