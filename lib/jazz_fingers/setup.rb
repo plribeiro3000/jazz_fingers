7cd0ecd6a357536f107a939bc7dfcc0c71a3e35d
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2015-06-27 12:11:42 -0300

    Fix loading of plugins

--- lib/jazz_fingers/setup.rb
+++ lib/jazz_fingers/setup.rb
@@ -2,6 +2,7 @@
 Pry.print = JazzFingers.print if JazzFingers.config.awesome_print?
 Pry.prompt = JazzFingers.prompt
 Pry.input = JazzFingers.input if JazzFingers.config.coolline?
+Pry.config.should_load_plugins = false
 
 # Command Shortcuts
 Pry.commands.alias_command("c", "continue")
